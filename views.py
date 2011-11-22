2bfb7ba5da86946cfdd06c42dd823f09853ac06a
Saxon Douglass <saxon.douglass@gmail.com>
2011-11-22 21:51:01 +1030

    purple colour scheme

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render_to_response
-import datetime
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    return render_to_response('current_datetime.html', {'current_date': now})
-
