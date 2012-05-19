2e25964a83f55617f1c52f29ccec3e7dd7b448c8
Finn Stokes <finn.stokes@gmail.com>
2012-05-19 13:31:39 +0930

    Finished mailer using email backend

--- mailer/views.py
+++ mailer/views.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.core.mail import EmailMessage
+from accounts.models import UserProfile
 
 def send_mail(request):
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
-            pass
+            message = EmailMessage(form.cleaned_data['subject'],
+                                   form.cleaned_data['body'],
+                                   'Jamalaide <contact@jamalaide.org.au>',
+                                   bcc = UserProfile.mailing_list())
+            message.send()
+            return render_to_response('mailer/sent_mail.html')
     else:
         form = EmailForm()
     
@@ -12,3 +19,7 @@ def send_mail(request):
     c.update(csrf(request))
     
     return render_to_response('mailer/send_mail.html', c)
+
+class EmailForm(forms.Form):
+    subject = forms.CharField()
+    body = forms.TextField()
