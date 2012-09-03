c0cac4e24b37a0cbeb9045d16026ab83032e349c
Saxon Douglass <saxon@saxondouglass.com>
2012-09-03 09:40:22 +0930

    Added DEBUG to local settings example.

--- jamalaide/local_settings.example.py
+++ jamalaide/local_settings.example.py
@@ -1,3 +1,7 @@
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',     # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
