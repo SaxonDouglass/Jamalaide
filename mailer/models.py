0441a353a6d96cfefc87d7f408d1e91f6ff4b58a
Finn Stokes <finn.stokes@gmail.com>
2015-05-13 15:46:46 +0930

    Fixed bugs with mailer in latest version of django

--- mailer/models.py
+++ mailer/models.py
@@ -2,19 +2,19 @@ from django import forms
 from django.conf import settings
 from django.db import models
 
+SAVED = "SA"
+SENT = "SE"
+ERROR = "ER"
+STATES = (
+    (SAVED, 'Saved'),
+    (SENT, 'Sent'),
+    (ERROR, 'Error'),
+)
+
 class Mail(models.Model):
     subject = models.CharField(max_length=69)
     body = models.TextField()
     recipients = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    
-    SAVED = "SA"
-    SENT = "SE"
-    ERROR = "ER"
-    STATES = (
-        (SAVED, 'Saved'),
-        (SENT, 'Sent'),
-        (ERROR, 'Error'),
-    )
     state = models.CharField(max_length=2,
                              choices=STATES,
                              default=SAVED)
@@ -22,4 +22,4 @@ class Mail(models.Model):
 class MailForm(forms.ModelForm):
     class Meta:
         model = Mail
-#        exclude = ('jam',)
+        fields = ('subject', 'body', 'recipients', 'state')
